91895b1f9e914938f57c8b2470a4b7a5fd0c04c0
george ayman <george.ayman97@gmail.com>
2022-12-23 16:41:05 +0200

    creating models and migrations

---
+++ app/Models/City.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class,'country_id');
+    }
+
+    public function populations(){
+        return $this->hasMany(Population::class,'city_id' , 'id');
+    }
+}
---
+++ database/migrations/2022_12_23_141832_create_populations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('populations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('year')->nullable();
+            $table->unsignedFloat('value')->nullable();
+            $table->string('sex');
+            $table->text('reliabilty');
+            $table->foreignId('city_id')->constrained('cities')->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('populations');
+    }
+};
